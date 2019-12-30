54f0089899137fcf4d4d317fe745cc6d98dcb20a
Ankur Nigam <ankur.nigam198@gmail.com>
2019-12-30 19:47:47 +0530

    Add project files.

---
+++ IdentityServer/Quickstart/Account/RegisterInputModel.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer4.Quickstart.UI
+{
+    public class RegisterInputModel
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Required]
+        public string[] Roles { get; set; }
+    }
+}
